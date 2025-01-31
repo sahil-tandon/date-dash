6a2050fea141b4b1479ae4aedfe7d97d63fd6361
Sahil Tandon <sahil.tandon@live.com>
2025-01-31 20:03:39 +0530

    Update page body background color.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-rose-100`}
       >
         <CloudBackground />
         <main className="relative min-h-screen">
